7e86551f75b25dbe274aeb54bd9514f8523ab069
unknown <krsmanovic013@gmail.com>
2023-05-04 19:37:04 +0200

    input forma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CustomForm from "./components/custom-form";
 
-function App() {
+const App = () => {
+  const addTask = (task) => {
+    console.log(task);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>My Task List</h1>
       </header>
+      <CustomForm addTask={addTask} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/custom-form.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { PlusIcon } from "@heroicons/react/24/solid";
+
+const CustomForm = ({ addTask }) => {
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    addTask({
+      name: task,
+      checked: false,
+      id: Date.now(),
+    });
+    setTask("");
+  };
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const [task, setTask] = useState("");
+
+  return (
+    <form className="todo" onSubmit={handleFormSubmit}>
+      <div className="wrapper">
+        <input
+          type="text"
+          id="task"
+          className="input"
+          value={task}
+          onChange={handleChange}
+          required
+          autoFocus
+          maxLength={60}
+          placeholder="Enter Task"
+        />
+        <label htmlFor="task" className="label">
+          ENTER TASK
+        </label>
+      </div>
+      <button className="btn" aria-label="Add Task" type="submit">
+        <PlusIcon />
+      </button>
+    </form>
+  );
+};
+
+export default CustomForm;
